5cfce46f8516fe9f23951a01d1212eceb54f64e0
D-Cipher <d.cipher.x@gmail.com>
2016-04-10 11:51:49 -0400

    4/10/16

--- IOS Apps/Hello World/ParseStarterProject/ChatTabController.swift
+++ IOS Apps/Hello World/ParseStarterProject/ChatTabController.swift
@@ -13,18 +13,75 @@ import CoreData
 
 class ChatTabController: UIViewController {
     
-    var userProfileData = Dictionary<String,AnyObject>()
-    
-    var userProfileImages = Dictionary<String,AnyObject>()
-    
     var context: NSManagedObjectContext?
 
+    private var fetchedResultsController: NSFetchedResultsController?
+    
+    private let tableView = UITableView(frame: CGRectZero, style: .Plain)
+    
+    private let cellIdentifier = "MessageCell"
+    
+    func newChat(){
+        
+    }
+    
+    func fakeData(){ //For testing
+        guard let context = context else {return}
+        let chat = NSEntityDescription.insertNewObjectForEntityForName("Chat", inManagedObjectContext: context) as? Chat
+    }
+    
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        let cell = cell as! ChatCell
+        guard let chat = fetchedResultsController?.objectAtIndexPath(indexPath) as? Chat else {return}
+        
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "MM/dd/YY"
+        cell.nameLabel.text = "Eliot"
+        cell.dateLabel.text = formatter.stringFromDate(NSDate())
+        cell.messageLabel.text = "Hey!!!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Get user Profile Preferences from Parse
-        //if user exists in parse then save userprofiledata to perm data
-        //if perm data is not empty then
+        //Set up Nav Bar
+        title = "Friends"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "contact_book"), style: .Plain, target: self, action: "newChat")
+        automaticallyAdjustsScrollViewInsets = false
+        
+        //Set up Chat Table
+        tableView.registerClass(ChatCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        
+        let tableViewConstraints: [NSLayoutConstraint] = [
+            tableView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+            tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+            tableView.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor)
+        ]
+        
+        NSLayoutConstraint.activateConstraints(tableViewConstraints)
+        
+        //Set up Core Data Fetching Context
+        if let context = context {
+            let request = NSFetchRequest(entityName: "Chat")
+            request.sortDescriptors = [NSSortDescriptor(key: "lastMessageTime", ascending: false)]
+            fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+            
+            fetchedResultsController?.delegate = self //set up extention for fetched results controller
+            
+            do {
+                try fetchedResultsController?.performFetch()
+            } catch {
+                print("error fetching")
+            }
+        }
+        
+        fakeData() //For testing
         
     }
 
@@ -40,9 +97,84 @@ class ChatTabController: UIViewController {
             let msgVC = nav.topViewController as! MessageViewController
             msgVC.context = context
         }
-        
-        
+    }
+}
+
+extension ChatTabController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return fetchedResultsController?.sections?.count ?? 0
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = fetchedResultsController?.sections else {return 0}
+        
+        let currentSection = sections[section]
+        return currentSection.numberOfObjects
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        configureCell(cell, atIndexPath: indexPath)
+        
+        return cell
+    }
+}
 
+extension ChatTabController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        guard let chat = fetchedResultsController?.objectAtIndexPath(indexPath) as? Chat else {return}
+        
+    }
+}
+
+extension ChatTabController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        switch type {
+        case .Insert: tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+        
+        case .Delete: tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+            
+        default: break
+        }
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        switch type {
+            
+        case .Insert: tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        
+        case .Update:
+            let cell = tableView.cellForRowAtIndexPath(indexPath!)
+            configureCell(cell!, atIndexPath: indexPath!)
+            tableView.reloadRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            
+        case .Move:
+            tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+            
+        case .Delete:
+            tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.endUpdates()
+    }
 }
